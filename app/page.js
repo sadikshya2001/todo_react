82d8b34a14bd34dc6e81796ce83ea1c75abf5c77
Sadikshya Satyal <sadikshyasatyal50@gmail.com>
2024-10-20 20:18:43 +0300

    final changes

--- app/page.js
+++ app/page.js
@@ -36,7 +36,7 @@ const page = () => {
             onClick={ ()=> {
               deleteHandler(i)
             }} 
-            className='bg-red-400 text-white px-4 py-2 rounded font-bold'>Delete
+            className='bg-green-400 text-white px-4 py-2 rounded font-bold'>DONE
           </button>
         </li>
       );
@@ -48,7 +48,7 @@ const page = () => {
     <>
     <h1 className='bg-black text-white p-5 text-5xl font-bold text-center'>Todo App</h1>
 
-    <form onSubmit={submitHandler}>
+    <form onSubmit={submitHandler} className='flex items-center justify-center mx-auto space-x-4 w-full'>
 
       <input 
         type='text' 
@@ -64,7 +64,7 @@ const page = () => {
       <input 
         type='text' 
         className='text-2xl border-zinc-800 border-4 m-8 px-4 py-2' 
-        placeholder='Enter description of your task'
+        placeholder='Enter description here'
         value={desc}
         onChange={(e)=> {
           setdesc(e.target.value)
